469334d8de9d8e81abf09246e0c67a7b56ddf125
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-12-07 20:03:08 +0800

    Second commit

--- [CMPE201] Linked List Merging.py
+++ [CMPE201] Linked List Merging.py
@@ -3,10 +3,69 @@ class Node:
     def __init__(self, data):
         self.data  = data
         self.next = None
+
+#Links the created lists
+def link_linked_lists(list1, list2):
+    if not list1:
+        return list2
+    if not list2:
+        return list1
+    current = list1
+    while current.next:
+        current = current.next
+    current.next = list2
+    return list1
+
+def merge(list1, list2):
+    dummy = Node(0)
+    current = dummy
+
+    while list1 is not None and list2 is not None:
+        if list1.data < list2.data:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+
+        current = current.next
+
+    if list1 is not None:
+        current.next = list1
+    elif list2 is not None:
+        current.next = list2
+
+    return dummy.next
+
+#Sorts the created lists
+def merge_sort_linked_list(head):
+    if head is None or head.next is None:
+        return head
+
+    mid = find_middle(head)
+    second_half = mid.next
+    mid.next = None
+
+    sorted_first_half = merge_sort_linked_list(head)
+    sorted_second_half = merge_sort_linked_list(second_half)
+
+    return merge(sorted_first_half, sorted_second_half)
+
+def find_middle(head):
+    slow = head
+    fast = head
+
+    while fast.next is not None and fast.next.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+
 #Create a new stack
 class Stack:
     def __init__(self):
         self.top = None
+
     #Methods of the stack (peek, pop, push)
     def push(self, data):
         new_node=Node(data)
@@ -27,6 +86,19 @@ class Stack:
         else:
             return None
         
-#Sample initialization
-stack=Stack()
-#Merging of two linked lists
+list1 = Node(3)
+list1.next = Node(1)
+list1.next.next = Node(4)
+
+list2 = Node(2)
+list2.next = Node(5)
+list2.next.next = Node(6)
+
+merged_list = link_linked_lists(list1, list2)
+
+sorted_merged_list = merge_sort_linked_list(merged_list)
+
+current = sorted_merged_list
+while current is not None:
+    print(current.data, end=" -> ")
+    current = current.next
